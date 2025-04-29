cf5bb2759f2f79b5abca938f706c0a2530eb7632
Kristina Faye <hellokristinafaye@gmail.com>
2025-04-29 16:19:21 -0700

    testing stuff out for OdinxFCC

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("skinny legend")
+
+
+const hand = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * hand.length)
+    console.log(choice);
+    console.log(hand[choice]);
+    return hand[choice]
+}
+
+getComputerChoice();
